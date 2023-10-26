81ca55398a7e8dba0874a781cb66f31aca25ff2a
Andrew <pzkpfwiilu4@gmail.com>
2023-10-26 22:10:59 +0300

    broke the sqlaclchemy
    yep

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,56 @@
-from flask import Flask, render_template, redirect, flash, request
-import os
-
-# from wtforms import Form, BooleanField, StringField, PasswordField, validators
-# from wtforms.validators import DataRequired
+from flask import Flask, render_template, redirect, flash, request, session, g, url_for
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 from app import forms
-from app import db
-
-# class LoginForm(Form):
-#    username = StringField('Username', [validators.Length(min=4, max=25)])
-#    remember_me = BooleanField('Remember this name', default=False)
-# email = StringField('Email Address', [validators.Length(min=6, max=35)])
-# password = PasswordField('New Password', [
-#    validators.DataRequired(),
-#    validators.EqualTo('confirm', message='Passwords must match')
-# ])
-# confirm = PasswordField('Repeat Password')
-# accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
-# class ChangePassword(Form):
-# password = PasswordField('New Password', [InputRequired(), EqualTo('confirm', message='Passwords must match')])
-# confirm  = PasswordField('Repeat Password')
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, logout_user
 
 
-def get_values():
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'qwert'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = '/login'
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
+
+@app.route('/')
+@app.route('/index')
+def index1():
+    return render_template('index.html')
+
+
+@app.route('/index2')
+def index2():
+    return render_template('index2.html')
+
+
+@app.route('/index3')
+def index3():
+    return render_template('index3.html')
+
+
+@app.route("/exchange_rates")
+def exchange_rates():  # можно сунуть в отдельный файл
     import requests
     from bs4 import BeautifulSoup
     import re
@@ -30,73 +59,72 @@ def get_values():
     requests.get(link)
     page = BeautifulSoup(requests.get(link).text, 'html.parser')
     values = page.find_all('div', class_='col-md-2 col-xs-9 _right mono-num')
-    prices = [1, 2, 3]
-    price1 = ''
-    price2 = ''
-    price3 = ''
+    prices = []
+    ''
+    ''
+    ''
 
     for i in range(len(values)):
         prices.append(values[i].text)
         prices[i] = re.sub(r"[\n\t\s]*", "", prices[i])
-        price1 = f"Yuan - {prices[0:2]} buy/sell"
-        price2 = f"Dollar - {prices[2:4]} buy/sell"
-        price3 = f"Euro - {prices[4:-1]} buy/sell"
-        prices = [price1, price2, price3]
-    return price1, price2, price3
-
-
-app = Flask(__name__)
-app.config.from_mapping(
-    SECRET_KEY='123',
-    DATABASE=os.path.join(app.instance_path, 'somedb.sqlite'),
-)  # hui poimet kak rabotaet
-
-
-@app.route("/")
-@app.route("/start")
-def main_menu():  # можно сунуть в отдельный файл
+    price1 = f"{prices[0] + ' ' + prices[1]} buy/sell".replace('[]', '')
+    price2 = f"{prices[2] + ' ' + prices[3]} buy/sell".replace('[]', '')
+    price3 = f"{prices[4] + ' ' + prices[5]} buy/sell".replace('[]', '')
     user = {'nickname': 'asda'}
     post = [
         {
-            'value': {'currency': 'Yuan'},
-            'body': "price1"  # видимо, необходимо заполнение из базы данных.
+            'value': {'currency': 'Yuan, CNY'},
+            'body': price1  # видимо, необходимо заполнение из базы данных.
         },
         {
-            'value': {'currency': 'Dollar'},
-            'body': "price2"
+            'value': {'currency': 'Dollar, USD'},
+            'body': price2
         },
         {
-            'value': {'currency': 'Euro'},
-            'body': "price3"
+            'value': {'currency': 'Euro, EUR'},
+            'body': price3
         }
     ]
-    return render_template("mainmenu.html",
+    return render_template("exchange_rates.html",
                            title='a',
                            user=user,
                            post=post)
-
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# !!!проверить установленные библиотеки, поставить, если что где нужно(по учебнику)!!!
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-@app.route('/test/', methods=['POST', 'GET'])
-def test():
-    message = ''
-    username = request.form.get('username')  # запрос к данным формы
+@app.route('/register', methods=["GET", 'POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
     password = request.form.get('password')
-    #user_id = 0
-    #if username:      # not working
-    #    user_id += 1;
-    #    db.c.execute(f"""
-    #INSERT INTO data_from_form VALUES
-    #    ({user_id},{username}),
-    #    """)
-    if username == 'root' and password == 'pass':  # captcha-database could work like this: we take a pic ind a key for
-        # it to display on screen
-        message = "Correct username and password"
-        return redirect('/start')
-    else:
-        message = 'a' # define the way to not get to False at any circumstances
+
+    user = User.query.filter_by(
+        email=email).first()  # if this returns a user, then the email already exists in database
+
+    if user:  # if a user is found, we want to redirect back to signup page so user can try again
+        return redirect(url_for('/signup'))
+
+    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('login')  # might have to do different one here
 
 
-    return render_template('test.html', message=message)
+@app.route('/login', methods=["GET", 'POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('/login'))  # if the user doesn't exist or password is wrong, reload the page
+    # login code goes here
+    login_user(user, remember=remember)
+    return redirect(url_for('/'))
+
--- app/db.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect('hella_db.db')
-c = con.cursor()
-
